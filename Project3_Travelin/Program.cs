e2c970d7dca95e8afd0086933d690050b1072e2c
Beyza Rabia Kılıçaslan <beyzarabia121@gmail.com>
2026-02-12 18:23:45 +0300

    2.ders sonu

--- Project3_Travelin/Program.cs
+++ Project3_Travelin/Program.cs
@@ -1,4 +1,27 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver.Core.Operations;
+using Project3_Travelin.Services.CategoryServices;
+using Project3_Travelin.Services.CommentServices;
+using Project3_Travelin.Services.TourServices;
+using Project3_Travelin.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICommentService, CommentService>(); //DI mantýðý ICommentService istenildiðinde CommentService sunuluyor (metotlarýyla)== best practices
+builder.Services.AddScoped<ICategoryService, CategoryService>(); 
+builder.Services.AddScoped<ITourService, TourService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); //AutoMapper’ýn kullanýlabilmesi için uygulamaya ekleniyor
+                                                                 //- Projedeki tüm Profile sýnýflarýný (AutoMapper konfigürasyonlarýný) bulup yükler.
+
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings")); //- appsettings.json içindeki "DatabaseSettings" bölümünü alýyor. 
+
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
---
+++ Project3_Travelin/Services/TourServices/TourService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using Project3_Travelin.DTOs.TourDTOs;
+using Project3_Travelin.Entities;
+using Project3_Travelin.Settings;
+
+namespace Project3_Travelin.Services.TourServices
+{
+    public class TourService : ITourService
+    {
+        private readonly IMapper _mapper;
+        private readonly IMongoCollection<Tour> _tourCollection;
+
+        public TourService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString); //mongo dbye bağlantı adresini çekiyoruz
+            var database = client.GetDatabase(_databaseSettings.DatabaseName); //kullanılacak veritabanı adını alıyoruz
+            _tourCollection = database.GetCollection<Tour>(_databaseSettings.TourCollectionName); //kullanılacak tablo
+            _mapper = mapper;
+        }
+
+        public async Task CreateTourAsync(CreateTourDTO createTourDTO)
+        {
+            var values = _mapper.Map<Tour>(createTourDTO);
+            await _tourCollection.InsertOneAsync(values);
+        }
+
+        public async Task DeleteTourAsync(string id)
+        {
+           await _tourCollection.DeleteOneAsync(x=>x.TourId ==id);
+        }
+
+        public async Task<List<ResultTourDTO>> GetAllTourAsync()
+        {
+            var values = await _tourCollection.Find(x=>true).ToListAsync();
+            return _mapper.Map<List<ResultTourDTO>>(values);
+        }
+
+        public async Task<GetTourByIdDTO> GetTourByIdAsync(string id)
+        {
+            var value = await _tourCollection.Find(x => x.TourId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetTourByIdDTO>(value);
+        }
+
+        public async Task UpdateTourAsync(UpdateTourDTO updateTourDTO)
+        {
+            var values = _mapper.Map<Tour>(updateTourDTO);
+            await _tourCollection.FindOneAndReplaceAsync(x=>x.TourId ==updateTourDTO.TourId, values);
+        }
+    }
+}
